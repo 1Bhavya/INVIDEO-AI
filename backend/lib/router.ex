5a9939ebed03f2f7aa4e23090810f1d202cd982f
Bhavya Ameriya <ameriyabhavya@gmail.com>
2025-02-26 00:51:04 +0530

    Ho gaya bas

--- backend/lib/router.ex
+++ backend/lib/router.ex
@@ -2,7 +2,7 @@ defmodule ShaderBackend.Router do
   use Plug.Router
 
   plug Corsica,
-    origins: [ "https://invideo-ai.vercel.app",  "http://localhost:3000", "http://localhost:4000", "http://127.0.0.1:3000", "http://127.0.0.1:4000", "*"],  # Allow requests from any origin
+    origins: [ "https://invideo-ai.vercel.app", "https://invideo-ai.onrender.com",  "http://localhost:3000", "http://localhost:4000", "http://127.0.0.1:3000", "http://127.0.0.1:4000"],  # Allow requests from any origin
     allow_headers: ["content-type"],  # Allow the Content-Type header
     allow_credentials: true  # Allow POST requests
     allow_methods: [:post]     
